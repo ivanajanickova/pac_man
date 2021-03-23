9e11b451972fe20a8d1d72d7e23c13a3b9c2ddd8
antonella <antonella@localhost>
2021-03-23 20:20:17 +0100

    PacMan class implemented.

--- pac_man/src/pacman/PacMan.java
+++ pac_man/src/pacman/PacMan.java
@@ -1,23 +1,98 @@
 package pacman;
 
 /**
+ * @mutable
  * Each instance of this class represents the player-controlled Pac-Man character in a Pac-Man maze.
+ * @invar | getSquare()!= null
+ * @invar |	getNbLives() >= 0
  */
 public class PacMan {
 	
-	public Square getSquare() { throw new RuntimeException("Not yet implemented"); }
+	/**
+	 * @representationObject
+	 * @invar | nbLives >=0
+	 */
 	
-	public int getNbLives() { throw new RuntimeException("Not yet implemented"); }
-
-	public PacMan(int nbLives, Square square) {
-		throw new RuntimeException("Not yet implemented");
+	private int nbLives;
+	
+	/**
+	 * @representationObject
+	 * @invar square != null
+	 */
+	
+	private Square square;
+	
+	/**
+	 * Returns the current square where the maze is found.
+	 * @basic
+	 */
+	
+	public Square getSquare() { 
+		return square;
 	}
 	
-	public void setSquare(Square square) { throw new RuntimeException("Not yet implemented"); }
+	/**
+	 * Returns the current number of lives that the maze has left.
+	 * @basic
+	 */
+	
+	public int getNbLives() { 
+		return nbLives;
+	}
+	
+	/**
+	 * @inspects | this
+	 * 
+	 * @pre | square != null
+	 * @pre | nbLives >0
+	 * 
+	 * @post | nbLives >=0
+	 * @post | nbLives == getNbLives()
+	 * @post | square == getSquare()
+	 */
+
+	public PacMan(int nbLives, Square square) {
+		if (square == null) {
+			throw new IllegalArgumentException("`square` is null");}
+		if (nbLives < 0) {
+			throw new IllegalArgumentException("`nbLives` is a negative number");}
+
+		this.nbLives= nbLives;
+		this.square=square;
+	}
+	
+	/**
+	 * @mutates | this
+	 * @inspects | other
+	 * @throws IllegalArgumentException | square == null
+	 */
+	
+	//Something about checking that the new square is not out of the maze map ?
+	
+	public void setSquare(Square square) { 
+		if (square == null) {
+			throw new IllegalArgumentException("`square` is null");}
+		this.square = square;
+		
+
+	}
 	
 	/**
 	 * Decreases this Pac-Man character's number of lives by one.
+	 * @mutates | this
+	 * 
+	 * @throws IllegalArgumentException | nbLives < 1
+	 * 
+	 * @pre | nbLives >=0 
+	 * @post | this.nbLives == old(nbLives) - 1
 	 */
-	public void die() { throw new RuntimeException("Not yet implemented"); }
+	
+	public void die() { 
+		if (nbLives < 1) {
+			throw new IllegalArgumentException("`nbLives` is 0");}
+		this.nbLives = nbLives - 1;
+		
+	}
+
 
 }
