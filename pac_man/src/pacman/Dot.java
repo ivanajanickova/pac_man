fdf973c67a75182522a7d8767ad42a512d0ae094
ivanajanickova <ivana.janickova97@gmail.com>
2021-03-24 21:20:56 +0100

    Dot class implementation + test suite

--- pac_man/src/pacman/Dot.java
+++ pac_man/src/pacman/Dot.java
@@ -2,11 +2,40 @@ package pacman;
 
 /**
  * Each instance of this class represents a dot (a piece of food for Pac-Man) in a Pac-Man maze.
+ * @immutable 
+ * 
+ * @invar | getSquare() != null
+ * 
  */
 public class Dot {
 	
-	public Square getSquare() { throw new RuntimeException("Not yet implemented"); }
+	/**
+	 * @invar | square != null
+	 * 
+	 * @representationObject
+	 */
+	private Square square;
 	
-	public Dot(Square square) { throw new RuntimeException("Not yet implemented"); }
+	/**
+	 * @basic
+	 */
+	public Square getSquare() { 
+		return Square.of(square.getMazeMap(), square.getRowIndex(), square.getColumnIndex()); 
+	}
+	
+	
+	/**
+	 * @inspects | square
+	 * 
+	 * @throws IllegalArgumentException | square == null
+	 * 
+	 * @post | getSquare().equals(square)
+	 */
+	public Dot(Square square) { 
+		if (square == null) {
+			throw new IllegalArgumentException("`square` is null.");
+		}
+		this.square = Square.of(square.getMazeMap(), square.getRowIndex(), square.getColumnIndex()); 
+	}
 
 }
