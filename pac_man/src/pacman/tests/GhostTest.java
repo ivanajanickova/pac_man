bc4de2392e36e01349b6711bdc05151ca9452580
antonella <antonella@localhost>
2021-03-25 18:01:41 +0100

    Ghost class implementation and test suite.

---
+++ pac_man/src/pacman/tests/GhostTest.java
@@ -0,0 +1,38 @@
+package pacman.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+import org.junit.jupiter.api.Test;
+
+import pacman.MazeMap;
+import pacman.PacMan;
+import pacman.Square;
+import pacman.Dot;
+import pacman.Ghost;
+import pacman.Direction;
+
+class GhostTest {
+
+	@Test
+	void test() {
+		MazeMap map = new MazeMap(3, 2, new boolean[] {false, true, true, false, true, true});
+		Square square = Square.of(map, 1, 0);
+		Direction direction = Direction.LEFT;
+		
+		Ghost ghost = new Ghost(square, direction);
+		assertEquals(true, Square.of(map, 1, 0).equals(ghost.getSquare()));
+		assertEquals(true, direction.equals(ghost.getDirection()));
+		
+		Square square2 = Square.of(map, 1, 1);
+		ghost.setSquare(square2);
+		assertEquals(true, ghost.getSquare().equals(square2));
+		
+		Direction direction2 = Direction.RIGHT;
+		ghost.setDirection(direction2);
+		assertEquals(true, ghost.getDirection().equals(direction2));
+
+
+	}
+}
